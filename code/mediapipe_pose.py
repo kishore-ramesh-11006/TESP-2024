39b2ac7475f35740121e20f53acfc9212db3a836
Izabela Trepáčová <xtrepacova@stuba.sk>
2024-07-29 16:00:44 +0900

    Update mediapipe_pose.py

--- code/mediapipe_pose.py
+++ code/mediapipe_pose.py
@@ -15,7 +15,7 @@ wanted_pose_landmarks = [
     ]
 
 is_hand_in_frame = False
-receive = False
+goal_is_reached = False
 def main(use_socket=False, ip="127.0.0.1", port=8000):
   # For webcam input:
   cap = cv2.VideoCapture(0)
@@ -63,119 +63,122 @@ def main(use_socket=False, ip="127.0.0.1", port=8000):
         cv2.putText(image, "1", (int(width/2) - (sizeText[0]//2), (int(height/2)+sizeText[1])),cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 2)
 
       else:
-        image.flags.writeable = False
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        results = pose.process(image)
+        while not goal_is_reached:
+          image.flags.writeable = False
+          image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+          results = pose.process(image)
 
-        # Draw the pose annotation on the image.
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        
-        # Draw grid on image
-        cv2.line(image,(0,int(height/2)),(int(width),int(height/2)),(255,0,0),5)
-        cv2.line(image,(int(width/2),0),(int(width/2),int(height)),(255,0,0),5)
-
-        #Draw landmark on the wrist
-        if results.pose_landmarks:
-                  for landmark in wanted_pose_landmarks:
-                      landmark_pos = results.pose_landmarks.landmark[landmark.value]
-                      x, y = int(landmark_pos.x * width), int(landmark_pos.y * height)
-                      cv2.circle(image, (x, y), 30, (0, 255, 0), -1)
-
-        if results.pose_landmarks:
-          data = get_joint_angles(results)
-        else:
-          data = [0,0,0]
-        
-        # Flip the image horizontally for a selfie-view display.
-        image = cv2.flip(image, 1)
-        # tutorial arrows
-        if (time.time() - start_time) >= 7 and (time.time() - start_time) < 17:
-          #arrow pointing down
-          start_point = (int(width * 0.1), int(height * 0.55))
-          end_point = (int(width * 0.1), int(height * 0.55) + int(height * 0.2))
-          color = (0, 255, 0)  
-          thickness = 3
-          cv2.arrowedLine(image, start_point, end_point, color, thickness, tipLength=0.2)
-          # text next to the arrow
-          sizeText = cv2.getTextSize("BACKWARD", cv2.FONT_HERSHEY_PLAIN, 1, 1)[0]
-          sizeText = cv2.getTextSize("BACKWARD", cv2.FONT_HERSHEY_PLAIN, 1, 1)[0]
-          cv2.rectangle(image, (int(width * 0.1) - (sizeText[0]//2) - 6, int(height * 0.55) + (int(height * 0.25)) + sizeText[1] + 6),
-                                    (int(width * 0.1) + (sizeText[0]//2) + 6, int(height * 0.55) + int(height * 0.25) - sizeText[1]//2 - 6), (0, 255, 0),
-                                    thickness=cv2.FILLED)
-          cv2.putText(image, "BACKWARD", (int(width * 0.1) - (sizeText[0]//2), (int(height * 0.55) + int(height * 0.25) +sizeText[1])),
-                      cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
-
-          #arrow pointing up 
-          start_point = (int(width * 0.1), int(height * 0.45))
-          end_point = (int(width * 0.1), int(height * 0.45) - int(height * 0.2))
-          color = (0, 255, 0)  
-          thickness = 3
-          # text next to the arrow
-          sizeText = cv2.getTextSize("FORWARD", cv2.FONT_HERSHEY_PLAIN, 1, 1)[0]
-          sizeText = cv2.getTextSize("FORWARD", cv2.FONT_HERSHEY_PLAIN, 1, 1)[0]
-          cv2.rectangle(image, (int(width * 0.1) - (sizeText[0]//2) - 6, int(height * 0.45) - (int(height * 0.25)) + sizeText[1] + 6),
-                                    (int(width * 0.1) + (sizeText[0]//2) + 6, int(height * 0.45) - int(height * 0.25) - sizeText[1]//2 - 6), (0, 255, 0),
-                                    thickness=cv2.FILLED)
-          cv2.putText(image, "FORWARD", (int(width * 0.1) - (sizeText[0]//2), (int(height * 0.45) - int(height * 0.25) +sizeText[1])),
-                      cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
-
-
-          cv2.arrowedLine(image, start_point, end_point, color, thickness, tipLength=0.2)
-          #arrow pointing left 
-          start_point = (int(width * 0.45), int(height * 0.9))
-          end_point = (int(width * 0.45) - int(height * 0.2), int(height * 0.9))
-          color = (0, 255, 0) 
-          thickness = 3
-          cv2.arrowedLine(image, start_point, end_point, color, thickness, tipLength=0.2)
-          # text next to the arrow
-          sizeText = cv2.getTextSize("LEFT", cv2.FONT_HERSHEY_PLAIN, 1, 1)[0]
-          cv2.rectangle(image, (int(width * 0.45) - int(height * 0.2) - (sizeText[0]//2) - 6, int(height * 0.9) - (int(height * 0.08)) + sizeText[1] + 6),
-                                    (int(width * 0.45) - int(height * 0.2) + (sizeText[0]//2) + 6, int(height * 0.9) - int(height * 0.08) - sizeText[1]//2 - 6), (0, 255, 0),
-                                    thickness=cv2.FILLED)
-          cv2.putText(image, "LEFT", (int(width * 0.45) - int(height * 0.2) - (sizeText[0]//2), (int(height * 0.9) - int(height * 0.08) +sizeText[1])),
-                      cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
+          # Draw the pose annotation on the image.
+          image.flags.writeable = True
+          image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
           
-          #arrow pointing right
-          start_point = (int(width * 0.55), int(height * 0.9))
-          end_point = (int(width * 0.55) + int(height * 0.2), int(height * 0.9))
-          color = (0, 255, 0)  
-          thickness = 3
-          cv2.arrowedLine(image, start_point, end_point, color, thickness, tipLength=0.2)
-          # text next to the arrow
-          sizeText = cv2.getTextSize("RIGHT", cv2.FONT_HERSHEY_PLAIN, 1, 1)[0]
-          cv2.rectangle(image, (int(width * 0.55) + int(height * 0.2) - (sizeText[0]//2) - 6, int(height * 0.9) - (int(height * 0.08)) + sizeText[1] + 6),
-                                    (int(width * 0.55) + int(height * 0.2) + (sizeText[0]//2) + 6, int(height * 0.9) - int(height * 0.08) - sizeText[1]//2 - 6), (0, 255, 0),
-                                    thickness=cv2.FILLED)
-          cv2.putText(image, "RIGHT", (int(width * 0.55) + int(height * 0.2) - (sizeText[0]//2), (int(height * 0.9) - int(height * 0.08) +sizeText[1])),
-                      cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
-          
+          # Draw grid on image
+          cv2.line(image,(0,int(height/2)),(int(width),int(height/2)),(128,128,128),5)
+          cv2.line(image,(int(width/2),0),(int(width/2),int(height)),(128,128,128),5)
 
-        # Border for signalization if the hand is in the frame
-        if not data[2]: # not in the frame
-          image = cv2.copyMakeBorder(image, 20, 20, 20, 20, cv2.BORDER_CONSTANT, value=[0, 0, 255]) 
-          
-          sizeText = cv2.getTextSize("PUT RIGHT HAND IN TO THE FRAME", cv2.FONT_HERSHEY_PLAIN, 1.5, 2)[0]
+          #Draw landmark on the wrist
+          if results.pose_landmarks:
+                    for landmark in wanted_pose_landmarks:
+                        landmark_pos = results.pose_landmarks.landmark[landmark.value]
+                        x, y = int(landmark_pos.x * width), int(landmark_pos.y * height)
+                        cv2.circle(image, (x, y), 30, (0, 255, 0), -1)
 
-          cv2.rectangle(image, (int(width/2) - (sizeText[0]//2) - 6, int(height/2) + sizeText[1] + 6),
-                                    (int(width/2) + (sizeText[0]//2) + 6, int(height/2) - sizeText[1]//2 - 6), (0, 0, 255),
-                                    thickness=cv2.FILLED)
-          cv2.putText(image, "PUT RIGHT HAND IN TO THE FRAME", (int(width/2) - (sizeText[0]//2), (int(height/2)+sizeText[1])),
-                      cv2.FONT_HERSHEY_PLAIN, 1.5, (255, 255, 255), 2)
-        else:
-          image = cv2.copyMakeBorder(image, 20, 20, 20, 20, cv2.BORDER_CONSTANT, value=[128, 128, 128]) 
+          if results.pose_landmarks:
+            data = get_joint_angles(results)
+          else:
+            data = [0,0,0]
+          
+          # Flip the image horizontally for a selfie-view display.
+          image = cv2.flip(image, 1)
+          # tutorial arrows
+          if (time.time() - start_time) >= 7 and (time.time() - start_time) < 17:
+            #arrow pointing down
+            start_point = (int(width * 0.1), int(height * 0.55))
+            end_point = (int(width * 0.1), int(height * 0.55) + int(height * 0.2))
+            color = (0, 255, 0)  
+            thickness = 3
+            cv2.arrowedLine(image, start_point, end_point, color, thickness, tipLength=0.2)
+            # text next to the arrow
+            sizeText = cv2.getTextSize("BACKWARD", cv2.FONT_HERSHEY_PLAIN, 1, 1)[0]
+            sizeText = cv2.getTextSize("BACKWARD", cv2.FONT_HERSHEY_PLAIN, 1, 1)[0]
+            cv2.rectangle(image, (int(width * 0.1) - (sizeText[0]//2) - 6, int(height * 0.55) + (int(height * 0.25)) + sizeText[1] + 6),
+                                      (int(width * 0.1) + (sizeText[0]//2) + 6, int(height * 0.55) + int(height * 0.25) - sizeText[1]//2 - 6), (0, 255, 0),
+                                      thickness=cv2.FILLED)
+            cv2.putText(image, "BACKWARD", (int(width * 0.1) - (sizeText[0]//2), (int(height * 0.55) + int(height * 0.25) +sizeText[1])),
+                        cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
+
+            #arrow pointing up 
+            start_point = (int(width * 0.1), int(height * 0.45))
+            end_point = (int(width * 0.1), int(height * 0.45) - int(height * 0.2))
+            color = (0, 255, 0)  
+            thickness = 3
+            # text next to the arrow
+            sizeText = cv2.getTextSize("FORWARD", cv2.FONT_HERSHEY_PLAIN, 1, 1)[0]
+            sizeText = cv2.getTextSize("FORWARD", cv2.FONT_HERSHEY_PLAIN, 1, 1)[0]
+            cv2.rectangle(image, (int(width * 0.1) - (sizeText[0]//2) - 6, int(height * 0.45) - (int(height * 0.25)) + sizeText[1] + 6),
+                                      (int(width * 0.1) + (sizeText[0]//2) + 6, int(height * 0.45) - int(height * 0.25) - sizeText[1]//2 - 6), (0, 255, 0),
+                                      thickness=cv2.FILLED)
+            cv2.putText(image, "FORWARD", (int(width * 0.1) - (sizeText[0]//2), (int(height * 0.45) - int(height * 0.25) +sizeText[1])),
+                        cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
+
+            cv2.arrowedLine(image, start_point, end_point, color, thickness, tipLength=0.2)
+            #arrow pointing left 
+            start_point = (int(width * 0.45), int(height * 0.9))
+            end_point = (int(width * 0.45) - int(height * 0.2), int(height * 0.9))
+            color = (0, 255, 0) 
+            thickness = 3
+            cv2.arrowedLine(image, start_point, end_point, color, thickness, tipLength=0.2)
+            # text next to the arrow
+            sizeText = cv2.getTextSize("LEFT", cv2.FONT_HERSHEY_PLAIN, 1, 1)[0]
+            cv2.rectangle(image, (int(width * 0.45) - int(height * 0.2) - (sizeText[0]//2) - 6, int(height * 0.9) - (int(height * 0.08)) + sizeText[1] + 6),
+                                      (int(width * 0.45) - int(height * 0.2) + (sizeText[0]//2) + 6, int(height * 0.9) - int(height * 0.08) - sizeText[1]//2 - 6), (0, 255, 0),
+                                      thickness=cv2.FILLED)
+            cv2.putText(image, "LEFT", (int(width * 0.45) - int(height * 0.2) - (sizeText[0]//2), (int(height * 0.9) - int(height * 0.08) +sizeText[1])),
+                        cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
+            
+            #arrow pointing right
+            start_point = (int(width * 0.55), int(height * 0.9))
+            end_point = (int(width * 0.55) + int(height * 0.2), int(height * 0.9))
+            color = (0, 255, 0)  
+            thickness = 3
+            cv2.arrowedLine(image, start_point, end_point, color, thickness, tipLength=0.2)
+            # text next to the arrow
+            sizeText = cv2.getTextSize("RIGHT", cv2.FONT_HERSHEY_PLAIN, 1, 1)[0]
+            cv2.rectangle(image, (int(width * 0.55) + int(height * 0.2) - (sizeText[0]//2) - 6, int(height * 0.9) - (int(height * 0.08)) + sizeText[1] + 6),
+                                      (int(width * 0.55) + int(height * 0.2) + (sizeText[0]//2) + 6, int(height * 0.9) - int(height * 0.08) - sizeText[1]//2 - 6), (0, 255, 0),
+                                      thickness=cv2.FILLED)
+            cv2.putText(image, "RIGHT", (int(width * 0.55) + int(height * 0.2) - (sizeText[0]//2), (int(height * 0.9) - int(height * 0.08) +sizeText[1])),
+                        cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
+            
+
+          # Border for signalization if the hand is in the frame
+          if not data[2]: # not in the frame
+            image = cv2.copyMakeBorder(image, 20, 20, 20, 20, cv2.BORDER_CONSTANT, value=[0, 0, 255]) 
+            
+            sizeText = cv2.getTextSize("PUT RIGHT HAND IN TO THE FRAME", cv2.FONT_HERSHEY_PLAIN, 1.5, 2)[0]
+
+            cv2.rectangle(image, (int(width/2) - (sizeText[0]//2) - 6, int(height/2) + sizeText[1] + 6),
+                                      (int(width/2) + (sizeText[0]//2) + 6, int(height/2) - sizeText[1]//2 - 6), (0, 0, 255),
+                                      thickness=cv2.FILLED)
+            cv2.putText(image, "PUT RIGHT HAND IN TO THE FRAME", (int(width/2) - (sizeText[0]//2), (int(height/2)+sizeText[1])),
+                        cv2.FONT_HERSHEY_PLAIN, 1.5, (255, 255, 255), 2)
+          else:
+            image = cv2.copyMakeBorder(image, 20, 20, 20, 20, cv2.BORDER_CONSTANT, value=[0, 255, 0]) 
       
-      cv2.imshow('Output', image)
+        cv2.imshow('Output', image)
 
-      if use_socket:
-        serialized_data = pickle.dumps(data)
-        client_socket.send(serialized_data)  
-        receive = client_socket.recv(4096)
-        receive = pickle.loads(receive)
-        
-
-      if cv2.waitKey(5) & 0xFF == 27:
-        break
+        if use_socket:
+          serialized_data = pickle.dumps(data)
+          client_socket.send(serialized_data)  
+          receive = client_socket.recv(4096)
+          goal_is_reached = pickle.loads(receive)
+          
+        if cv2.waitKey(5) & 0xFF == 27:
+          break
+      
+      image[:] = (0, 0, 0)
+      sizeText = cv2.getTextSize("GAME OVER", cv2.FONT_HERSHEY_PLAIN, 2, 2)[0]
+      cv2.putText(image, "GAME OVER", (int(width/2) - (sizeText[0]//2), (int(height/2)+sizeText[1])),cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
 
   cap.release()
 
@@ -205,7 +208,7 @@ def get_joint_angles(results):
   return [right_wrist[0],right_wrist[1],is_hand_in_frame]
 
 if __name__ == "__main__":
-  use_socket = False
+  use_socket = True
   ip = "127.0.0.1"
   port = 8000
   main(use_socket=use_socket, ip=ip, port=port)
